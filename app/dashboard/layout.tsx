33a4ffe010731b0ad9b2643bf1f024aa516b04df
Ardian Maulana <exbeelh@gmail.com>
2024-05-20 09:31:17 +0700

    add header in layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import SideNav from '../ui/dashboard/sidenav';
+import { UserCircleIcon } from '@heroicons/react/24/outline';
 
 export default function RootLayout({
   children,
@@ -11,7 +12,17 @@ export default function RootLayout({
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex h-full flex-grow flex-col">
+        <div className="h-8 flex-none border-spacing-1 bg-white p-6 md:p-12">
+          <div className="mb-5 flex flex-row items-center justify-end leading-none">
+            <UserCircleIcon className="h-8 w-8" />
+            <p>
+              Hello, <span className="font-bold">Person</span>
+            </p>
+          </div>
+        </div>
+        <div className="flex-grow overflow-y-auto p-6 md:p-12">{children}</div>
+      </div>
     </div>
   );
 }
